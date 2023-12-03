bb6f85d18638efcd0cbb6ed7f8a1e4e4adf57bfa
Idris Raja <idris.raja@gmail.com>
2023-12-03 17:06:39 -0600

    update flake and readme

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ My goals for this year is to use nix and haskell with a focus on
 * nix flakes
 * optics
 * parsers
+
+To run any solution via nix
+
+```
+nix run github:idrisr/advent2023#<day><part> <input>
+
+# for example
+# run day 2 part 1
+nix run github:idrisr/advent2023#02p1 my-input-file
+
+# run day 3 part 2
+nix run github:idrisr/advent2023#03p2 my-input-file
+```
--- flake.nix
+++ flake.nix
@@ -6,59 +6,47 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
-    let
-      system = flake-utils.lib.system.x86_64-linux;
-      compiler = "ghc927";
-      pkgs = import nixpkgs { inherit system; };
-      hPkgs = pkgs.haskell.packages."${compiler}";
-      myDevTools = with hPkgs; [
-        ghc
-        ghcid
-        fourmolu
-        hlint
-        hoogle
-        haskell-language-server
-        implicit-hie
-        retrie
-        cabal-install
-        cabal2nix
-        pkgs.zlib
-        hspec
-      ];
-      day01 = with pkgs; haskell.packages.${compiler}.callPackage ./01 { };
-      day02 = with pkgs; haskell.packages.${compiler}.callPackage ./02 { };
-      day03 = with pkgs; haskell.packages.${compiler}.callPackage ./03 { };
-    in {
-      packages.${system} = { inherit day01 day02 day03; };
-      apps.${system} = {
-        day01p1 = {
-          type = "app";
-          program = "${day01}/bin/part1";
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        compiler = "ghc927";
+        pkgs = import nixpkgs { inherit system; };
+        hPkgs = pkgs.haskell.packages."${compiler}";
+        myDevTools = with hPkgs; [
+          ghc
+          ghcid
+          fourmolu
+          hlint
+          hoogle
+          haskell-language-server
+          implicit-hie
+          retrie
+          cabal-install
+          cabal2nix
+          pkgs.zlib
+          hspec
+        ];
+        day01 = with pkgs; haskell.packages.${compiler}.callPackage ./01 { };
+        day02 = with pkgs; haskell.packages.${compiler}.callPackage ./02 { };
+        day03 = with pkgs; haskell.packages.${compiler}.callPackage ./03 { };
+        mkApp = day: part:
+          {
+            program = "${day}/bin/part${part}";
+          } // {
+            type = "app";
+          };
+      in {
+        packages = { inherit day01 day02 day03; };
+        apps = {
+          day01p1 = mkApp "${day01}" "1";
+          day01p2 = mkApp "${day01}" "2";
+          day02p1 = mkApp "${day02}" "1";
+          day02p2 = mkApp "${day02}" "2";
+          day03p1 = mkApp "${day03}" "1";
+          day03p2 = mkApp "${day03}" "2";
         };
-        day01p2 = {
-          type = "app";
-          program = "${day01}/bin/part2";
+        devShells.default = pkgs.mkShell {
+          buildInputs = myDevTools;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath myDevTools;
         };
-        day02p1 = {
-          type = "app";
-          program = "${day02}/bin/part1";
-        };
-        day02p2 = {
-          type = "app";
-          program = "${day02}/bin/part2";
-        };
-        day03p1 = {
-          type = "app";
-          program = "${day03}/bin/part1";
-        };
-        day03p2 = {
-          type = "app";
-          program = "${day03}/bin/part2";
-        };
-      };
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = myDevTools;
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath myDevTools;
-      };
-    };
+      });
 }
